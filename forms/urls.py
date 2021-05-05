06789b87a27a8e0a1c18e2a1ccf209622dad9fd4
AptYam <anikronjon.swe@gmail.com>
2021-05-05 12:01:19 +0600

    Registration View Created

--- forms/urls.py
+++ forms/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'forms'
 urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('registration', views.registration_view, name='registration'),
 
 ]
--- forms/views.py
+++ forms/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import UserRegistrationForm
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
+def home_view(request):
+    return render(request, 'forms/homepage.html', {'name': request.user})
+
+
+def registration_view(request):
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = UserRegistrationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                newcomer = authenticate(
+                    username=form.cleaned_data['username'],
+                    password=form.cleaned_data['password1']
+                )
+                login(request, newcomer)
+                return HttpResponseRedirect('dashbord/')
+        else:
+            form = UserRegistrationForm()
+        return render(request, 'forms/registration.html', {'form': form, 'name': request.user})
+
